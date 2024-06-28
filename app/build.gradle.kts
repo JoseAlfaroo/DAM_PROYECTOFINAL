0ee579fd6e0ca2ffc3e649009f33fbdad167e3b0
mals24 <marceloliendo24@gmail.com>
2024-06-28 14:38:51 -0500

    Implementación de Dependencias para Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.facebook.android:facebook-android-sdk:latest.release")
+    
+    implementation("com.squareup.retrofit2:retrofit:2.5.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
 
 }
